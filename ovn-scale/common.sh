b6e46f110807a36388ba48f9a32af854f78f7e65
Dumitru Ceara <dceara@redhat.com>
2019-07-04 10:54:10 +0200

    Increase max number of PIDs for ovn-scale setups
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- ovn-scale/common.sh
+++ ovn-scale/common.sh
@@ -6,7 +6,12 @@ source /vagrant/utils/common-functions
 mkdir -p /root/.ssh/
 cp /home/vagrant/.ssh/authorized_keys /root/.ssh/authorized_keys
 
+# Allow lots and lots of pids (in case we want to use lots of iperf instances)
+echo 1000000 > /proc/sys/kernel/pid_max
+
 install_ovs
+
+yum install psmisc -y
 
 # Install EPEL repo
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
